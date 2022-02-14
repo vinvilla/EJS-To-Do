67d6e36b285a5674fcf1b83ee966dcbee3b49db5
vinvilla <93366146+vinvilla@users.noreply.github.com>
2022-02-13 21:39:10 -0800

    Add files via upload

---
+++ date.js
@@ -0,0 +1,48 @@
+exports.getDate = function() {
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+}
+
+exports.getDay = function() {
+  const today = new Date();
+
+  const options = {
+    weekday: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+}
+
+
+// module.exports.getDate = getDate;
+//
+// function getDate() {
+//   let today = new Date();
+//
+//   let options = {
+//     weekday: "long",
+//     day: "numeric",
+//     month: "long"
+//   };
+//
+//   return today.toLocaleDateString("en-US", options);
+// }
+//
+// module.exports.getDay = getDay;
+//
+// function getDay() {
+//   let today = new Date();
+//
+//   let options = {
+//     weekday: "long"
+//   };
+//
+//   return today.toLocaleDateString("en-US", options);
+// }
